df4cdec395f9e0c6618358e1b3bd9a7fc8744a74
Dongkeun <hongdongk@naver.com>
2024-08-11 22:10:43 +0900

    상단 배경 fade-out 설정, hover이벤트 수정

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 interface HeaderProps {
   scrolled: string;
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,38 +1,62 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import top from '../assets/top.jpg';
 import NavigationBar from './NavigationBar';
+import React, { useState, useEffect } from 'react';
+
+interface BackgroundProps {
+  opacity: number;
+}
 
 export default function Top() {
+  const [opacity, setOpacity] = useState(1);
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    const maxScroll = window.innerHeight;
+    const newOpacity = Math.max(1 - scrollPosition / maxScroll, 0); // (0 : 완전히 투명)
+    setOpacity(newOpacity);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [opacity]);
+
   const scrollToPosition = () => {
     window.scrollTo({
-      top: 420,
+      top: 470,
       behavior: 'smooth',
     });
   };
 
   return (
-    <Content>
+    <>
       <NavigationBar />
-      <Middle>
-        <p>FrontEnd Developer Portfolio</p>
-        <SubTitle>홍동근</SubTitle>
-      </Middle>
-      <Bottom>
-        <BottomButton onClick={scrollToPosition}>
-          자세히 알아보기 ↓
-        </BottomButton>
-      </Bottom>
-    </Content>
+      <Content opacity={opacity}>
+        <Middle>
+          <p>FrontEnd Developer Portfolio</p>
+          <SubTitle>홍동근</SubTitle>
+        </Middle>
+        <Bottom>
+          <BottomButton onClick={scrollToPosition}>
+            자세히 알아보기 ↓
+          </BottomButton>
+        </Bottom>
+      </Content>
+    </>
   );
 }
 
-const Content = styled.div`
+const Content = styled.div<BackgroundProps>`
   width: 100vw;
-  height: 500px;
+  height: 550px;
   background-image: url(${top});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 75%;
+  transition: opacity 0.5s ease;
+  opacity: ${({ opacity }) => opacity};
 `;
 
 const Middle = styled.div`
@@ -58,6 +82,18 @@ const Bottom = styled.div`
   justify-content: center;
 `;
 
+const moveArrow = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(7px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
 const BottomButton = styled.div`
   width: 140px;
   height: 50px;
@@ -72,6 +108,24 @@ const BottomButton = styled.div`
   color: #fff;
   cursor: pointer;
   &:hover {
-    background-color: #b22222;
+    animation: ${moveArrow} 1s infinite;
   }
 `;
+
+// const BottomButton = styled.div`
+//   display: inline-block;
+//   width: 30px;
+//   height: 30px;
+//   border: 5px solid red;
+//   border-left: 5px solid red;
+//   border-bottom: 5px solid red;
+//   animation: ${moveArrow} 1s infinite;
+//   margin: 50px auto;
+//   img {
+//     width: 100%;
+//     height: 100%;
+//     background-color: white;
+//     font-weight: bold;
+//     font-size: 18px;
+//   }
+// `;
